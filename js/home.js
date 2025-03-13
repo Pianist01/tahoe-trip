1f1c775edb3029478d1d467520beaba9daf2efd3
Steven Peralta <155492295+Pianist01@users.noreply.github.com>
2025-03-13 16:30:14 -0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,1 +1,16 @@
 console.log('Script working');
+
+const navIcon = document.querySelector('.nav-container');
+const nav = document.querySelector('.navigation');
+const navExit = document.querySelector('.exit');
+
+navIcon.addEventListener('click', (e) => {
+    e.preventDefault();
+    nav.style.transform = 'scaleY(1)';
+    console.log('click');
+});
+
+navExit.addEventListener('click', (e) => {
+    e.preventDefault()
+    nav.style.transform = 'scaleY(0)';
+});
