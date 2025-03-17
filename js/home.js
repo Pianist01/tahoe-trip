8f130d3a435ba49b6f508eec9772df11b8cb6b2f
Steven Peralta <155492295+Pianist01@users.noreply.github.com>
2025-03-17 16:31:29 -0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -7,10 +7,43 @@ const navExit = document.querySelector('.exit');
 navIcon.addEventListener('click', (e) => {
     e.preventDefault();
     nav.style.transform = 'scaleY(1)';
-    console.log('click');
 });
 
 navExit.addEventListener('click', (e) => {
     e.preventDefault()
     nav.style.transform = 'scaleY(0)';
+});
+
+const eatPage = document.querySelector('.eat-page');
+const naturePage = document.querySelector('.nature-page');
+const funPage = document.querySelector('.fun-page');
+const explorePage = document.querySelector('.explore-page');
+const photoPage = document.querySelector('.photo-page');
+
+const links = document.querySelectorAll('.page-link');
+
+console.log(links);
+
+links.forEach((link, index) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log('a has been clicked');
+        console.log(`Clicked at ${index}`);
+        if(index === 0) {
+          eatPage.style.transform = 'scaleX(1)';
+          console.log('Style one');
+        } else if(index === 1) {
+            naturePage.style.transform = 'scaleX(1)';
+            console.log('Style two');
+        } else if(index === 2) {
+            funPage.style.transform = 'scaleX(1)';
+            console.log('Style three');
+        } else if(index === 3) {
+            explorePage.style.transform = 'scaleX(1)';
+            console.log('Style four');
+        } else if(index === 4) {
+            photoPage.style.transform = 'scaleX(1)';
+            console.log('Style five')
+        }
+    });
 });
